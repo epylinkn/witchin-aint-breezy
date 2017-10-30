472baf365711ebcc36f41dfb361dc9f3136139b3
Anthony Bui <anthonybui@gmail.com>
2017-10-30 08:21:14 -0400

    Accelo is working!!!!

--- witchin-aint-breezy.ino
+++ witchin-aint-breezy.ino
@@ -13,7 +13,7 @@ int pot2 = A1;
 int statusPin = 3;
 int buttonPin = 13;
 bool prevButtonState;
-bool dmpOn = true;
+int sensorMode = 0;
 int serialByte = 0;
 int pos = 0;
 int topServoAngle = 90;
@@ -29,6 +29,8 @@ uint8_t fifoBuffer[64]; // FIFO storage buffer
 Quaternion q;           // [w, x, y, z]         quaternion container
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+float baseYaw;
+float prevYaw = 0;
 
 
 void setup() {
@@ -37,7 +39,7 @@ void setup() {
   pinMode(buttonPin, INPUT);
   pinMode(statusPin, OUTPUT);
 
-  
+
   // join I2C bus (I2Cdev library doesn't do this automatically)
   #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
       Wire.begin();
@@ -45,7 +47,7 @@ void setup() {
   #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
       Fastwire::setup(400, true);
   #endif
-    
+
   Serial.begin(38400);
   mpu.initialize();
 
@@ -58,9 +60,9 @@ void setup() {
   devStatus = mpu.dmpInitialize();
 
    // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(220);
-  mpu.setYGyroOffset(76);
-  mpu.setZGyroOffset(-85);
+  mpu.setXGyroOffset(0);
+  mpu.setYGyroOffset(0);
+  mpu.setZGyroOffset(0);
   mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
 
   // make sure it worked (returns 0 if so)
@@ -92,19 +94,51 @@ void loop() {
   // 1. Button to toggle between accelerometer and potentiometers
   int buttonState = digitalRead(buttonPin);
   if (buttonState != prevButtonState && buttonState == HIGH) {
-    dmpOn = !dmpOn;
+    sensorMode = (sensorMode + 1) % 3; // 0 - dmp "calibration"; 1 - dmpOn; 2 - potentiometers
   }
   prevButtonState = buttonState;
 
-  if (dmpOn && !dmpReady) {
+  if (sensorMode == 0) {
+    Serial.println(F("Calibrating!"));
+
     digitalWrite(statusPin, LOW);
     delay(300);
     digitalWrite(statusPin, HIGH);
     delay(300);
-    return;
-  }
 
-  if (dmpOn) {
+    // get INT_STATUS byte
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+      // wait for correct available data length, should be a VERY short wait
+      while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+      // read a packet from FIFO
+      mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+      // track FIFO count here in case there is > 1 packet available
+      // (this lets us immediately read more without waiting for an interrupt)
+      fifoCount -= packetSize;
+
+      // display Euler angles in degrees
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+      baseYaw = ypr[0] * 180/M_PI;  // [-180, 180]
+    }
+
+  } else if (sensorMode == 1) {
     digitalWrite(statusPin, HIGH);
 
     // get INT_STATUS byte
@@ -123,64 +157,69 @@ void loop() {
     } else if (mpuIntStatus & 0x02) {
       // wait for correct available data length, should be a VERY short wait
       while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-  
+
       // read a packet from FIFO
       mpu.getFIFOBytes(fifoBuffer, packetSize);
-      
+
       // track FIFO count here in case there is > 1 packet available
       // (this lets us immediately read more without waiting for an interrupt)
       fifoCount -= packetSize;
-  
+
       // display Euler angles in degrees
       mpu.dmpGetQuaternion(&q, fifoBuffer);
       mpu.dmpGetGravity(&gravity, &q);
       mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-      Serial.print("ypr\t");
-      Serial.print(ypr[0] * 180/M_PI);
-      Serial.print("\t");
-      Serial.print(ypr[1] * 180/M_PI);
-      Serial.print("\t");
-      Serial.println(ypr[2] * 180/M_PI);
+
+      float yaw = ypr[0] * 180/M_PI - baseYaw;  // [-180, 180]
+      if (yaw < -180) { yaw += 360; }   // weird lower limit
+      if (yaw > 180) { yaw -= 360; }    // weird upper limit
+
+
+      float tolerance = 5.0;
+      if (yaw < prevYaw - tolerance || yaw > prevYaw + tolerance) {
+        float botTargetAngle = map(yaw, -180, 180, 0, 180);
+
+
+        botServo.write(botTargetAngle);
+        delay(15);
+        prevYaw = yaw;
+      }
+
+      Serial.print("yaw:\t");
+      Serial.print(yaw);
+
+//      Serial.print("ypr\t");
+//      Serial.print(ypr[0] * 180/M_PI);
+//      Serial.print("\t");
+//      Serial.print(ypr[1] * 180/M_PI);
+//      Serial.print("\t");
+//      Serial.print(ypr[2] * 180/M_PI);
     }
-
-    
-//    topServo.write(topServoAngle);
-//    botServo.write(botServoAngle);
-
-//    delay(15);
-
-//    if (Serial.available() > 0) {
-//      serialByte = Serial.read();
-//      pos += 10;
-//      Serial.println(pos);
-//      topServo.write(pos);
-//    }
-
 
   } else {
     digitalWrite(statusPin, LOW);
 
     int val1 = analogRead(pot1);
     int val2 = analogRead(pot2);
-    
+
     Serial.print("val1 \t");
     Serial.print(val1);
     Serial.print("\tval2 \t");
     Serial.print(val2);
-    
+
     int mappedVal1 = map(val1, 0, 1023, 0, 180);
     int mappedVal2 = map(val2, 0, 1023, 0, 180);
-  
+
     Serial.print("\tmappedVal1 \t");
     Serial.print(mappedVal1);
     Serial.print("\tmappedVal2 \t");
     Serial.print(mappedVal2);
-    
+
     botServo.write(mappedVal1);
     topServo.write(mappedVal2);
     delay(15); // waits for the servo to get there
   }
-  
+
   Serial.println();
 }
 
